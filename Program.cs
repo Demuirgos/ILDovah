3bdf2da07ceb2cdfc015eae52144b0f60c386d3f
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2023-01-01 15:18:31 +0100

    Renamed Folders

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using static Core;
 
-// Note(Ayman) : Add whitespace skipping to all parsers, and add a parser for whitespace
-
 var (source, index) = (".property int32 Count() { .get instance int32 MyCount::get_Count() .set instance void MyCount::set_Count(int32) .other instance void MyCount::reset_Count() }", 0);
 
 TestConstruct<Property>(ref index, source);
