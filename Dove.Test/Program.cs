6eb74d2a2c629db51fe2e369dd0f96ed5059d515
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2023-01-18 09:39:27 +0100

    Added Todo

--- Dove.Test/Program.cs
+++ Dove.Test/Program.cs
@@ -1,9 +1,10 @@
 ﻿using Dove.Core;
 int index = 0;
-var result = RootDecl.Declaration.Collection.AsParser(File.ReadAllText("Test.il"), ref index, out var res, out string err);
+var result = MethodDecl.Method.AsParser(File.ReadAllText("Test.il"), ref index, out var res, out string err);
 
 // TODO : Add MSFT Specific stuff
 // TODO : Add TABLES 
 // TODO : Make Declaration base check in Source generator check the namespace
 // TODO : Optimize the parser it is slow
+// TODO : Check method Locals syntax (if Indexer of syntax "'['int32']' type [id]" is valid)
 Console.WriteLine(result  ? $"Success to parse {res.GetType().Name} =>  {res}" : $"Failed to parse {res?.GetType().Name} \n{err}");
